9007272a7b1cae027387e909cea67972f0e52714
xyzliu <535153924@qq.com>
2020-08-19 17:25:49 +0800

    基本数据类型-链表的增删改查及说明

--- BasicDataType/src/cn/xyzliu/linkedlist/MyLinkedList.java
+++ BasicDataType/src/cn/xyzliu/linkedlist/MyLinkedList.java
@@ -63,10 +63,11 @@ public class MyLinkedList {
             //获取插入位置前的节点
             Node preNode = getNode(index - 1);
             //新插入节点的next指针指向插入节点的位置
-            preNode.next = preNode.next;  //等同于preNode.next = getNode(index);
+            insertNode.next = preNode.next;  //等同于preNode.next = getNode(index);
             //插入位置前的节点的next指针指向新插入的节点
             preNode.next = insertNode;
         }
+        size++;  //链表节点数加1
     }
 
     /**
@@ -84,5 +85,84 @@ public class MyLinkedList {
             tmp = tmp.next;  //tmp表示当前节点的地址，tmp.next表示当前节点所指向下一节点的地址
         }
         return tmp;//返回查找到的节点
+    }
+
+    /**
+     * 修改节点数据
+     * @param data 修改后的数据
+     * @param index 需要修改的节点的下标
+     */
+    public void modify(int data, int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("超出链表节点范围！");
+        }
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        tmp.data = data;
+    }
+
+    /**
+     * 链表删除节点
+     * @param index 被删除节点的下标
+     * @return 返回被删除的节点
+     */
+    public Node remove(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("超出链表节点范围！");
+        }
+        Node removeNode = null;
+        if(index == 0){
+            //删除头节点  思路：将链表的头节点设为原先头节点的next指针即可
+            //将头节点赋值给removeNode以便返回给调用函数
+            removeNode = head;
+            //将链表的头节点设为原先头节点的next指针
+            head = head.next;
+        } else if(index == size - 1){
+            //删除尾节点  思路：将倒数第二个节点的next指针指向空即可
+
+            Node preNode = getNode(index - 1); //获取倒数二个节点
+            removeNode = preNode.next; //将要删除节点地址赋值给removeNode,以便返回给调用函数
+
+            //使得最后一个节点的地址赋值给last，使得last永远指向最后一个节点。
+            last = preNode;
+        } else{
+            //删除中间节点  思路：将要删除节点的前置节点的next指针指向要删除接节点的下一个指针即可
+
+            Node preNode = getNode(index - 1);  //获取前置节点
+            Node nextNode = preNode.next.next;  //获取后置节点
+            removeNode = preNode.next;  //将要删除节点地址赋值给removeNode,以便返回给调用函数
+            preNode.next = nextNode;
+        }
+        size--;  //链表节点数减1
+        return removeNode;
+    }
+
+    /**
+     * 输出链表所保存的数据
+     */
+    public void output(){
+        Node tmp = head;
+        while (tmp != null){
+            System.out.println(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.insert(3,0);
+        myLinkedList.insert(7,1);
+        myLinkedList.insert(9,2);
+        myLinkedList.insert(5,3);
+        myLinkedList.insert(6,1);
+        myLinkedList.output();
+        System.out.println("修改下标为0的节点的数据为10");
+        myLinkedList.modify(10,0);
+        myLinkedList.output();
+        System.out.println("删除下标为0的节点");
+        myLinkedList.remove(0);
+        myLinkedList.output();
     }
 }
